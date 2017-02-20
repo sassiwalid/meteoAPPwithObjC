fa475e997888d74fd780e8394cdded3b39d9990f
MacBook Pro <macbookpro@imac-de-macbook.local>
2017-02-20 11:36:42 +0100

    Initial Commit

---
+++ MeteoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeteoApp
+//
+//  Created by MacBook Pro on 20/02/2017.
+//  Copyright © 2017 MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeteoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeteoApp
+//
+//  Created by MacBook Pro on 20/02/2017.
+//  Copyright © 2017 MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
